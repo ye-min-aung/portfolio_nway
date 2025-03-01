1e73691ec0a8b2c275064ba41d3a944714f6597d
ye-min-aung <yeminaung0901@gmail.com>
2025-03-01 11:03:27 +0630

    initial portfolio project set up nway

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Index from './pages/index.vue';
+</script>
+<template>
+  <Index/>
+</template>
+<style>
+@import "./assets/styles.css";
+</style>
---
+++ src/pages/index.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+<template>
+  <h1>home</h1>
+</template>
+<style>
+@import "./assets/styles.css";
+</style>
