f1e963cd9f3e5e69b6289294a1424c85925997aa
ye-min-aung <yeminaung0901@gmail.com>
2025-05-11 20:32:04 +0630

    test

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'url'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-   resolve: {
+  resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
